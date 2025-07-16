e1b4da4826e5ee0bd7eba0e62b6780f32f4bdf5e
shivam vishwakarma <vishwakarmashivam2003@gmail.com>
2025-07-17 01:39:07 +0530

    fix marquee

--- src/components/BrandCarousel.tsx
+++ src/components/BrandCarousel.tsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay } from 'swiper/modules'
+import Marquee from "react-fast-marquee";
+
 
 // Import Swiper styles
 import 'swiper/css'
@@ -9,44 +11,27 @@ const BrandCarousel = () => {
         { name: 'Brand 1', image: '/logo/1.png' },
         { name: 'Brand 2', image: '/logo/2.png' },
         { name: 'Brand 3', image: '/logo/3.png' },
-        { name: 'Brand 4', image: '/logo/4.png'  },
-        { name: 'Brand 6', image: '/logo/5.png'  },
-           { name: 'Brand 1', image: '/logo/1.png' },
-                  { name: 'Brand 2', image: '/logo/2.png' },
+        { name: 'Brand 4', image: '/logo/4.png' },
+        { name: 'Brand 6', image: '/logo/5.png' },
+        { name: 'Brand 1', image: '/logo/1.png' },
+        { name: 'Brand 2', image: '/logo/2.png' },
 
     ]
 
     return (
         <div className="w-full bg-black ">
             <div className=" mx-auto px-4">
-                <Swiper effect="fade"
-  modules={[Autoplay]}
-  loop={true}
-
-  speed={3000}
-  autoplay={{
-    delay: 1,
-    disableOnInteraction: false,
-  }}
-  slidesPerView={5}
-  spaceBetween={30}
-                    breakpoints={{
-                        320: { slidesPerView: 2 },
-                        640: { slidesPerView: 3 },
-                        768: { slidesPerView: 4 },
-                        1024: { slidesPerView: 5 },
-                    }}
-                >
+                <Marquee gradient={true} gradientColor='black' gradientWidth={100}>
                     {brands.map((brand, index) => (
-                        <SwiperSlide key={index} className="flex justify-center items-center">
+                        <div key={index} className="flex justify-center items-center">
                             <img
                                 src={brand.image}
                                 alt={brand.name}
-                                className="h-30 object-contain"
+                                className="h-[14rem] object-contain"
                             />
-                        </SwiperSlide>
+                        </div>
                     ))}
-                </Swiper>
+                </Marquee>
             </div>
         </div>
     )
